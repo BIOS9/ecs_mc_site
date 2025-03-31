9e3264c95bc57c80d4e1d4cf5f272bf0b5d18596
sofa <babalark@proton.me>
2025-03-31 14:16:32 +1300

    chore: `prettier` formatting

--- src/cooltitle.js
+++ src/cooltitle.js
@@ -1,4 +1,6 @@
 const title = document.title;
 for (let i = 0; i < 10; i++) {
-  setTimeout(() => {document.title = title + "!".repeat(i);}, 500 * i);
+  setTimeout(() => {
+    document.title = title + "!".repeat(i);
+  }, 500 * i);
 }
--- src/copyonclick.js
+++ src/copyonclick.js
@@ -1,13 +1,15 @@
 for (const e of document.getElementsByClassName("copy-on-click")) {
-	e.onclick = () => {
-		if (e.textContent != null) {
-			navigator.clipboard.writeText(e.textContent);
+  e.onclick = () => {
+    if (e.textContent != null) {
+      navigator.clipboard.writeText(e.textContent);
       const deleteMe = document.createElement("span");
       deleteMe.className = "copied";
       deleteMe.innerText = "(Copied!)";
       e.insertAdjacentElement("afterend", deleteMe);
 
-      setTimeout(()=>{ deleteMe.parentNode.removeChild(deleteMe); }, 2000);
-		}
-	}
+      setTimeout(() => {
+        deleteMe.parentNode.removeChild(deleteMe);
+      }, 2000);
+    }
+  };
 }
